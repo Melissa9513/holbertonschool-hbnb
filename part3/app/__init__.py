be72799cc17f6c0cacf440eede508dd9ade95cf5
leyvsi <sylviecosta78500@gmail.com>
2026-03-12 13:12:29 +0100

    add part3 to __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -4,18 +4,26 @@ Main application factory and API initialization.
 """
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from hbnb.app.extensions import db
+
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+def create_app(config_class="config.DevelopmentConfig"):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    db.init_app(app)
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
-def create_app(config_class="config.DevelopmentConfig"):
-    """
-    Factory function to create and configure the Flask app.
-    """
-    app = Flask(__name__)
-    app.config.from_object(config_class)
     # Initialize the API with basic metadata and Swagger documentation path
     api = Api(app,
               version='1.0',
